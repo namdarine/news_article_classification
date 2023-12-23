385df5c98d143b9e120a3e7e4a88b04f7f53ca4f
namdarine <namdarine.code@gmail.com>
2023-12-23 17:28:14 -0600

    Add Author

--- normalization.py
+++ normalization.py
@@ -10,9 +10,10 @@ from gensim.models import Word2Vec
 
 
 class sentence_vectorize:
-    def __init__(self, title, description):
+    def __init__(self, title, description, author):
         self.title = title
         self.description = description
+        self.author = author
         self.model = None
         self.vectorizer = None
         self.tfidf_matrix = None
@@ -29,7 +30,7 @@ class sentence_vectorize:
         return tokens
     
     def train_model(self):
-        all_tokenized = [self.tokenize_text(text) for text in self.title + self.description]
+        all_tokenized = [self.tokenize_text(text) for text in self.title + self.description + self.author]
         all_tokenized_text = [" ".join(tokens) for tokens in all_tokenized]
         
         # Calculate TF-IDF
@@ -38,19 +39,18 @@ class sentence_vectorize:
         
         # Train the model
         self.model = Word2Vec(sentences=all_tokenized, vector_size=100, window=5, min_count=1, workers=4)
-
         self.word_vectors = {word: self.model.wv[word] for word in self.model.wv.index_to_key}
         
     def get_sentence_vectors(self):
         if self.model is None:
             raise ValueError("Model not trained. Call train_model() first")
         
-        # Get sentence vectors each sentence
+        # Get sentence vectors for each sentence
         sentence_vectors = []
         
-        for tokens in [self.tokenize_text(text) for text in self.title + self.description]:
+        for tokens in [self.tokenize_text(text) for text in self.title + self.description + self.author]:
             sentence_vectors.append([])
-        
+            
             for word in tokens:
                 if word in self.word_vectors and word in self.vectorizer.vocabulary_:
                     index_in_tfidf_matrix = tokens.index(word)
@@ -58,11 +58,11 @@ class sentence_vectorize:
                     tfidf_value = self.tfidf_matrix[index_in_tfidf_matrix, index_in_vocab]
                     vector = tfidf_value * self.word_vectors[word]  # Use TF-IDF value as weight
                     sentence_vectors[-1].append(vector)
-                    
+            
             if sentence_vectors[-1]:
                 sentence_vectors[-1] = np.mean(sentence_vectors[-1], axis=0)
         
-        return sentence_vectors[0] + sentence_vectors[1]
+        return sentence_vectors[0] + sentence_vectors[1] + sentence_vectors[2]
     
     def save_vectors_to_csv(self, output_csv):
         sentence_vectors = self.get_sentence_vectors()
@@ -71,9 +71,11 @@ class sentence_vectorize:
             'Original Title Text': list(self.title),
             'Title Sentence Vector': sentence_vectors[0],
             'Original Description Text': list(self.description),
-            'Description Sentence Vector': sentence_vectors[1]
-            })
-        df_output.to_csv(output_csv, index = True)
+            'Description Sentence Vector': sentence_vectors[1],
+            'Original Author Text': list(self.author),
+            'Author Sentence Vector': sentence_vectors[2]
+        })
+        df_output.to_csv(output_csv, index=True)
 
 
 csv_file_path = '/Users/namgyulee/Personal_Project/News_Article_Classification/news_data.csv'
@@ -84,6 +86,7 @@ df['Description'] = df['Description'].fillna('')
 
 title = df['Title']
 description = df['Description']
+author = df['Author'].astype(str)
 
 
 train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
@@ -91,11 +94,14 @@ train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 # Split Title and Description
 # They have same articles
 title_train_set = train_set['Title']
-title_test_set = test_set['Title']
 des_train_set = train_set['Description']
-des_test_set = test_set['Description']
+author_train_set = train_set['Author'].astype(str)
 
-sentence_vectorizer = sentence_vectorize(title_train_set, des_train_set)
+title_test_set = test_set['Title']
+des_test_set = test_set['Description']
+author_test_set = test_set['Author'].astype(str)
+
+sentence_vectorizer = sentence_vectorize(title_train_set, des_train_set, author_train_set)
 sentence_vectorizer.train_model()
 train_sentence_vectors = sentence_vectorizer.get_sentence_vectors()
 sentence_vectorizer.save_vectors_to_csv('train_sentence_vectors.csv')
@@ -118,5 +124,3 @@ print(f'Vector representation of "{word}" : {word_vector}\n')
 word_ava_vec = np.mean(word_vector)
 print(word_ava_vec)
 """
-
-
